f995e28cf65dfa447fe6df2d3df96ea68f94d191
Yang Xianglin <yxlyzxl@gmail.com>
2022-08-14 01:20:50 +0800

    implement EvalProjector for random segment

--- singleVis/projector.py
+++ singleVis/projector.py
@@ -1,9 +1,8 @@
 """The Projector class for visualization, serve as a helper module for evaluator and visualizer"""
 import os
-
+import json
 import numpy as np
 import torch
-from scipy.special import softmax
 
 class Projector:
     def __init__(self, vis_model, content_path, segments, device) -> None:
@@ -67,3 +66,31 @@ class ALProjector(Projector):
         self.vis_model.to(self.DEVICE)
         self.vis_model.eval()
         print("Successfully load the visualization model for Iteration {}...".format(iteration))
+
+class EvalProjector(Projector):
+    def __init__(self, vis_model, content_path, device, exp) -> None:
+        super().__init__(vis_model, content_path, None, device)
+        self.exp = exp
+        file_path = os.path.join(content_path, "Model", "exp_{}".format(exp), "segments.json")
+        with open(file_path, "r") as f:
+            self.segments = json.load(f)
+    
+    def load(self, iteration):
+        if iteration < self.current_range[0] or iteration > self.current_range[1]:
+            for i in range(len(self.segments)):
+                s = self.segments[i][0]
+                e = self.segments[i][1]
+                # range [s, e)
+                if iteration >= s and iteration <= e:
+                    idx = i
+                    break
+            file_path = os.path.join(self.content_path, "Model", "exp_{}".format(self.exp), "tnn_hybrid_{}.pth".format(idx))
+            save_model = torch.load(file_path, map_location=self.DEVICE)
+            self.vis_model.load_state_dict(save_model["state_dict"])
+            self.vis_model.to(self.DEVICE)
+            self.vis_model.eval()
+            self.current_range = (s, e)
+            print("Successfully load the visualization model for range ({},{}]...".format(s,e))
+        else:
+            print("Same range as current visualization model...")
+        
