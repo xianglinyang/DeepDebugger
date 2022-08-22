c8f71f73122ef5ca1b8faeee8a6f7befa771a43f
Yang Xianglin <yxlyzxl@gmail.com>
2022-08-23 02:23:05 +0800

    new method

--- singleVis/trajectory_manager.py
+++ singleVis/trajectory_manager.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.cluster import Birch
 # TODO random ignore
 
@@ -180,3 +181,72 @@ class FeedbackSampling(TrajectoryManager):
             scores = sample_rate[s_idxs]
             return s_idxs, scores
         return s_idxs
+
+class AnormalyDetector:
+    def __init__(self, uncertainty, embeddings_2d, cls_num, period=100, metric="a"):
+        """ trajectory manager with no feedback
+        Parameters
+        ----------
+        samples: ndarray, shape(train_num, repr_dim)
+        embeddings_2d : ndarray, shape (train_num, epoch_num, 2)
+            all 2d embeddings of representations by timevis
+        cls_num: int 
+            the number of classes to cluster
+        period: int
+            We only look at the last *period* epochs of trajectory
+        """
+        self.uncertainty = uncertainty
+        self.embeddings_2d = embeddings_2d
+        train_num,time_steps, _ = embeddings_2d.shape
+        self.train_num = train_num
+        self.time_steps = time_steps
+        self.period = period
+        self.metric = metric
+        self.cls_num = cls_num
+
+        self.v = self.embeddings_2d[:, -period:,:][:,1:,:] - self.embeddings_2d[:, -period:,:][:,:-1,:]
+        self.a = self.v[:,1:,:]-self.v[:,:-1,:]
+    
+    def clustered(self):
+        brc = Birch(n_clusters=self.cls_num)
+        if self.metric == "v":
+            brc.fit(self.v.reshape(self.train_num, -1))
+        elif self.metric == "a":
+            brc.fit(self.a.reshape(self.train_num, -1))
+        else:
+            print("Not a valid metric")
+        
+        self.predict_sub_labels = brc.labels_
+        self.suspect_clean = np.argsort(np.bincount(self.predict_sub_labels))[-3:]
+
+        # to be updated each time
+        self.cls_score = np.zeros(self.cls_num)
+        for cls in range(self.cls_num):
+            self.cls_score[cls] = 1 - np.sum(self.predict_sub_labels==cls)/self.train_num
+    
+    def sample_batch_init(self, budget):
+        scores = (self.uncertainty + self.cls_scores[self.predict_sub_labels])/2
+        norm_rate = scores/np.sum(scores)
+        s_idxs = np.random.choice(self.train_num, p=norm_rate, size=budget, replace=False)
+        return s_idxs, scores
+    
+    def sample_batch(self, acc_idxs, rej_idxs, budget):
+        s1 = self.uncertainty
+        s2 = self.cls_scores[self.predict_sub_labels]
+        X = np.concatenate((s1, s2), axis=1)
+
+        exp_idxs = np.concatenate((acc_idxs, rej_idxs), axis=0)
+        target_X = X[exp_idxs]
+        target_Y = np.zeros(len(exp_idxs))
+        target_Y[:len(acc_idxs)] = 1
+        krr = KernelRidge(alpha=1.0)
+        krr.fit(target_X, target_Y)
+        scores = krr.predict(X)
+
+        not_selected = np.setdiff1d(np.arange(self.train_num), exp_idxs)
+        remain_scores = scores[not_selected]
+        args = np.argsort(remain_scores)[-budget:]
+        selected_idxs = not_selected[args]
+        return selected_idxs, scores[selected_idxs]
+
+
