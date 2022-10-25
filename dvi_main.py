99528ab426c9e65a04aca381479125b180d9c0c1
Yang Xianglin <yxlyzxl@gmail.com>
2022-10-25 19:39:58 +0800

    support dvi

---
+++ dvi_main.py
@@ -0,0 +1,195 @@
+########################################################################################################################
+#                                                     DVI PARAMETERS                                                   #
+########################################################################################################################
+"""This serve as an example of DeepVisualInsight implementation in pytorch."""
+VIS_METHOD = "DVI" # DeepVisualInsight
+
+########################################################################################################################
+#                                                          IMPORT                                                      #
+########################################################################################################################
+import torch
+import sys
+import os
+import json
+import time
+import copy
+import numpy as np
+import argparse
+
+from torch.utils.data import DataLoader
+from torch.utils.data import WeightedRandomSampler
+from umap.umap_ import find_ab_params
+
+from singleVis.custom_weighted_random_sampler import CustomWeightedRandomSampler
+from singleVis.SingleVisualizationModel import SingleVisualizationModel
+from singleVis.losses import UmapLoss, ReconstructionLoss, TemporalLoss, DVILoss
+from singleVis.edge_dataset import DVIDataHandler
+from singleVis.trainer import DVITrainer
+from singleVis.data import NormalDataProvider
+from singleVis.spatial_edge_constructor import SingleEpochSpatialEdgeConstructor
+from singleVis.projector import DVIProjector
+from singleVis.eval.evaluator import Evaluator
+
+########################################################################################################################
+#                                                     LOAD PARAMETERS                                                  #
+########################################################################################################################
+parser = argparse.ArgumentParser(description='Process hyperparameters...')
+parser.add_argument('--content_path', type=str)
+args = parser.parse_args()
+
+CONTENT_PATH = args.content_path
+sys.path.append(CONTENT_PATH)
+from config import config
+
+# record output information
+now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time())) 
+sys.stdout = open(os.path.join(CONTENT_PATH, now+".txt"), "w")
+
+SETTING = config["SETTING"]
+CLASSES = config["CLASSES"]
+DATASET = config["DATASET"]
+PREPROCESS = config["VISUALIZATION"]["PREPROCESS"]
+GPU_ID = config["GPU"]
+EPOCH_START = config["EPOCH_START"]
+EPOCH_END = config["EPOCH_END"]
+EPOCH_PERIOD = config["EPOCH_PERIOD"]
+
+# Training parameter (subject model)
+TRAINING_PARAMETER = config["TRAINING"]
+NET = TRAINING_PARAMETER["NET"]
+LEN = TRAINING_PARAMETER["train_num"]
+
+# Training parameter (visualization model)
+VISUALIZATION_PARAMETER = config["VISUALIZATION"]
+LAMBDA = VISUALIZATION_PARAMETER["LAMBDA"]
+B_N_EPOCHS = VISUALIZATION_PARAMETER["BOUNDARY"]["B_N_EPOCHS"]
+L_BOUND = VISUALIZATION_PARAMETER["BOUNDARY"]["L_BOUND"]
+INIT_NUM = VISUALIZATION_PARAMETER["INIT_NUM"]
+ALPHA = VISUALIZATION_PARAMETER["ALPHA"]
+BETA = VISUALIZATION_PARAMETER["BETA"]
+MAX_HAUSDORFF = VISUALIZATION_PARAMETER["MAX_HAUSDORFF"]
+HIDDEN_LAYER = VISUALIZATION_PARAMETER["HIDDEN_LAYER"]
+S_N_EPOCHS = VISUALIZATION_PARAMETER["S_N_EPOCHS"]
+T_N_EPOCHS = VISUALIZATION_PARAMETER["T_N_EPOCHS"]
+N_NEIGHBORS = VISUALIZATION_PARAMETER["N_NEIGHBORS"]
+PATIENT = VISUALIZATION_PARAMETER["PATIENT"]
+MAX_EPOCH = VISUALIZATION_PARAMETER["MAX_EPOCH"]
+
+VIS_MODEL_NAME = VISUALIZATION_PARAMETER["VIS_MODEL_NAME"]
+EVALUATION_NAME = VISUALIZATION_PARAMETER["EVALUATION_NAME"]
+
+# define hyperparameters
+DEVICE = torch.device("cuda:{}".format(GPU_ID) if torch.cuda.is_available() else "cpu")
+
+import Model.model as subject_model
+net = eval("subject_model.{}()".format(NET))
+
+########################################################################################################################
+#                                                    TRAINING SETTING                                                  #
+########################################################################################################################
+# Define data_provider
+data_provider = NormalDataProvider(CONTENT_PATH, net, EPOCH_START, EPOCH_END, EPOCH_PERIOD, split=-1, device=DEVICE, classes=CLASSES,verbose=1)
+if PREPROCESS:
+    data_provider._meta_data()
+    if B_N_EPOCHS >0:
+        data_provider._estimate_boundary(LEN//10, l_bound=L_BOUND)
+
+# Define visualization models
+# TODO make the structure a hyperparameter
+model = SingleVisualizationModel(input_dims=512, output_dims=2, units=256, hidden_layer=HIDDEN_LAYER)
+# Define Losses
+negative_sample_rate = 5
+min_dist = .1
+_a, _b = find_ab_params(1.0, min_dist)
+umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a, _b, repulsion_strength=1.0)
+recon_loss_fn = ReconstructionLoss(beta=1.0)
+temporal_loss_fn = TemporalLoss()
+# Define Projector
+projector = DVIProjector(vis_model=model, content_path=CONTENT_PATH, vis_model_name=VIS_MODEL_NAME, device=DEVICE)
+
+start_flag = 1
+w_prev = dict(model.named_parameters())
+for param in w_prev.values():
+    param.requires_grad=False
+
+for iteration in range(EPOCH_START, EPOCH_END, EPOCH_PERIOD):
+    # Define DVI Loss
+    if start_flag:
+        criterion = DVILoss(umap_loss_fn, recon_loss_fn, temporal_loss_fn, lambd1=1.0, lambd2=0.0)
+        start_flag = 0
+    else:
+        criterion = DVILoss(umap_loss_fn, recon_loss_fn, temporal_loss_fn, lambd1=1.0, lambd2=1.0)
+    # Define training parameters
+    optimizer = torch.optim.Adam(model.parameters(), lr=.01, weight_decay=1e-5)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=.1)
+    # Define Edge dataset
+    t0 = time.time()
+    spatial_cons = SingleEpochSpatialEdgeConstructor(data_provider, iteration, S_N_EPOCHS, B_N_EPOCHS, N_NEIGHBORS)
+    edge_to, edge_from, probs, feature_vectors, attention = spatial_cons.construct()
+    t1 = time.time()
+
+    probs = probs / (probs.max()+1e-3)
+    eliminate_zeros = probs>1e-3
+    edge_to = edge_to[eliminate_zeros]
+    edge_from = edge_from[eliminate_zeros]
+    probs = probs[eliminate_zeros]
+
+    # save result
+    trainer.record_time("{}_time_{}.json".format(VIS_METHOD, VIS_MODEL_NAME), "complex_construction", str(iteration), t1-t0)
+    dataset = DVIDataHandler(edge_to, edge_from, feature_vectors, attention, w_prev)
+
+    n_samples = int(np.sum(S_N_EPOCHS * probs) // 1)
+    # chose sampler based on the number of dataset
+    if len(edge_to) > 2^24:
+        sampler = CustomWeightedRandomSampler(probs, n_samples, replacement=True)
+    else:
+        sampler = WeightedRandomSampler(probs, n_samples, replacement=True)
+    edge_loader = DataLoader(dataset, batch_size=1000, sampler=sampler)
+
+    ########################################################################################################################
+    #                                                       TRAIN                                                          #
+    ########################################################################################################################
+
+    trainer = DVITrainer(model, criterion, optimizer, lr_scheduler,edge_loader=edge_loader, DEVICE=DEVICE)
+
+    t2=time.time()
+    trainer.train(PATIENT, MAX_EPOCH)
+    t3 = time.time()
+
+    trainer.record_time("{}_time_{}.json".format(VIS_METHOD, VIS_MODEL_NAME), "training", str(iteration), t3-t2)
+    save_dir = os.path.join(data_provider.model_path, "Epoch_{}".format(iteration))
+    trainer.save(save_dir=save_dir, file_name="{}".format(VIS_MODEL_NAME))
+
+    w_prev = dict(model.named_parameters())
+    for param in w_prev.values():
+        param.requires_grad=False
+
+########################################################################################################################
+#                                                      VISUALIZATION                                                   #
+########################################################################################################################
+
+from singleVis.visualizer import visualizer
+
+vis = visualizer(data_provider, projector, 200, 10, CLASSES)
+save_dir = os.path.join(data_provider.content_path, "img")
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+for i in range(EPOCH_START, EPOCH_END+1, EPOCH_PERIOD):
+    vis.savefig(i, path=os.path.join(save_dir, "{}_{}_{}.png".format(DATASET, i, VIS_METHOD)))
+
+    
+########################################################################################################################
+#                                                       EVALUATION                                                     #
+########################################################################################################################
+
+EVAL_EPOCH_DICT = {
+    "mnist_full":[1,2,5,10,13,16,20],
+    "fmnist_full":[1,2,6,11,25,30,36,50],
+    "cifar10_full":[1,3,9,18,24,41,70,100,160,200]
+}
+eval_epochs = EVAL_EPOCH_DICT[DATASET]
+
+evaluator = Evaluator(data_provider, projector)
+
+for eval_epoch in eval_epochs:
+    evaluator.save_epoch_eval(eval_epoch, 15, temporal_k=5, file_name="{}".format(EVALUATION_NAME))
