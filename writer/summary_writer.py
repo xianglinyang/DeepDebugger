14bcd640d190aa5b5b3c87ba2fc2fd64e5e34609
Yang Xianglin <yxlyzxl@gmail.com>
2022-10-27 22:15:58 +0800

    fix dataloader

--- writer/summary_writer.py
+++ writer/summary_writer.py
@@ -50,14 +50,8 @@ class SummaryWriter(SummaryWriterAbstractClass):
         self.batch_size = batch_size
         self.num_worker = num_worker
 
-    def add_training_data(self, dataset):
-        #! Noted, dataset transform need to be test transform
-        dataloader = DataLoader(
-            dataset,
-            batch_size=self.bat,
-            num_workers=self.num_worker,
-            shuffle=False   # need to keep order, otherwise the index saved would be wrong
-        )
+    def add_training_data(self, dataloader):
+
         trainset_data = None
         trainset_label = None
         for batch_idx, (inputs, targets) in enumerate(dataloader):
@@ -70,18 +64,11 @@ class SummaryWriter(SummaryWriterAbstractClass):
                 trainset_label = targets
 
         training_path = os.path.join(self.log_dir, "Training_data")
-        os.makedirs(training_path)
+        os.makedirs(training_path, exist_ok=True)
         torch.save(trainset_data, os.path.join(training_path, "training_dataset_data.pth"))
         torch.save(trainset_label, os.path.join(training_path, "training_dataset_label.pth"))
 
-    def add_testing_data(self, dataset):
-        dataloader = DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_worker,
-            drop_last=False,
-            shuffle=False
-        )
+    def add_testing_data(self, dataloader):
         testset_data = None
         testset_label = None
         for batch_idx, (inputs, targets) in enumerate(dataloader):
@@ -92,21 +79,22 @@ class SummaryWriter(SummaryWriterAbstractClass):
                 testset_data = inputs
                 testset_label = targets
         testing_path = os.path.join(self.log_dir, "Testing_data")
-        os.makedirs(testing_path)
+        os.makedirs(testing_path, exist_ok=True)
         torch.save(testset_data, os.path.join(testing_path, "testing_dataset_data.pth"))
         torch.save(testset_label, os.path.join(testing_path, "testing_dataset_label.pth"))
     
-    def add_checkpoint_data(self, state_dict, idxs, prev_id):
+    def add_checkpoint_data(self, state_dict, idxs, id, prev_id):
         checkpoints_path = os.path.join(self.log_dir, "Model")
-        dirs = os.listdir(checkpoint_path)
-        C = 0
-        for dir in dirs:
-            if "C_" in dir:
-                C = max(C, int(dir.replace("C_", "")))
-        C += 1
+        os.makedirs(checkpoints_path, exist_ok=True)
+        # dirs = os.listdir(checkpoints_path)
+        # C = 0
+        # for dir in dirs:
+        #     if "C_" in dir:
+        #         C = max(C, int(dir.replace("C_", "")))
+        # C += 1
 
-        checkpoint_path = os.path.join(checkpoints_path, "C_".format(C))
-        os.makedirs(checkpoint_path)
+        checkpoint_path = os.path.join(checkpoints_path, "Epoch_{}".format(id))
+        os.makedirs(checkpoint_path, exist_ok=True)
         torch.save(state_dict, os.path.join(checkpoint_path, "subject_model.pth"))
         
         with open(os.path.join(checkpoint_path, "index.json"), "w") as f:
@@ -114,14 +102,14 @@ class SummaryWriter(SummaryWriterAbstractClass):
         
         # update iteration structure
         iteration_structure_path = os.path.join(self.log_dir, "iteration_structure.json")
-        if prev_id is None:
-            iter_s = [{"value": C, "name": "checkpoint", "pid": ""}]
+        if prev_id < 1:
+            iter_s = [{"value": id, "name": "Epoch", "pid": ""}]
             with open(iteration_structure_path, "w") as f:
                 json.dump(iter_s, f)
         else:
             with open(iteration_structure_path,encoding='utf8')as fp:
                 json_data = json.load(fp)
-                json_data.append({'value': C, 'name': 'checkpoint', 'pid': "{}".format(prev_id)})
+                json_data.append({'value': id, 'name': 'Epoch', 'pid': "{}".format(prev_id)})
             with open(iteration_structure_path,'w') as f:
                 json.dump(json_data, f)
                 f.close()
