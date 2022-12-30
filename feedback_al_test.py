f71399e44baebf50319cb059a274393903ab491f
Yang Xianglin <yxlyzxl@gmail.com>
2022-12-30 11:51:20 +0800

    refactor

---
+++ feedback_al_test.py
@@ -0,0 +1,181 @@
+import numpy as np
+import os
+import json
+import torch
+import pickle
+import argparse
+
+from singleVis.data import ActiveLearningDataProvider
+
+def add_noise(rate, acc_idxs, rej_idxs):
+    if rate == 0:
+        return acc_idxs, rej_idxs
+    acc_noise = np.random.choice(len(acc_idxs), size=len(acc_idxs)*rate//1)
+    acc_noise = acc_idxs[acc_noise]
+    new_acc = np.setdiff1d(acc_idxs, acc_noise)
+
+    rej_noise = np.random.choice(len(rej_idxs), size=len(rej_idxs)*rate//1)
+    rej_noise = rej_idxs[rej_noise]
+    new_rej = np.setdiff1d(rej_idxs, rej_noise)
+
+    new_acc = np.concatenate((new_acc, rej_noise), axis=0)
+    new_rej = np.concatenate((new_rej, acc_noise), axis=0)
+    return new_acc, new_rej
+
+
+def init_sampling(tm, method, round, budget):
+    print("Feedback sampling initialization ({}):".format(method))
+    rate = list()
+    for _ in range(round):
+        correct = np.array([]).astype(np.int32)
+        wrong = np.array([]).astype(np.int32)
+        
+        suggest_idxs, _ = tm.sample_batch_init(correct, wrong, budget)
+        suggest_idxs = ulb_idxs[suggest_idxs]
+        correct = np.intersect1d(suggest_idxs, ulb_wrong)
+        rate.append(len(correct)/budget)
+    print("Init success Rate:\t{:.4f}".format(sum(rate)/len(rate)))
+    return sum(rate)/len(rate)
+
+
+def feedback_sampling(tm, method, round, budget, noise_rate=0):
+    print("Feedback sampling ({}) with noise rate {}:".format(method, noise_rate))
+    rate = list()
+    correct = np.array([]).astype(np.int32)
+    wrong = np.array([]).astype(np.int32)
+    map_ulb =ulb_idxs.tolist()
+
+    map_acc_idxs = np.array([map_ulb.index(i) for i in correct]).astype(np.int32)
+    map_rej_idxs = np.array([map_ulb.index(i) for i in wrong]).astype(np.int32)
+    suggest_idxs, _ = tm.sample_batch_init(map_acc_idxs, map_rej_idxs, budget)
+    suggest_idxs = ulb_idxs[suggest_idxs]
+    correct = np.intersect1d(suggest_idxs, ulb_wrong)
+    wrong = np.setdiff1d(suggest_idxs, correct)
+    rate.append(len(correct)/budget)
+    # inject noise
+    correct, wrong = add_noise(noise_rate, correct, wrong)
+    for _ in range(round-1):
+        map_acc_idxs = np.array([map_ulb.index(i) for i in correct]).astype(np.int32)
+        map_rej_idxs = np.array([map_ulb.index(i) for i in wrong]).astype(np.int32)
+        suggest_idxs,_ = tm.sample_batch(map_acc_idxs, map_rej_idxs, budget)
+        suggest_idxs = ulb_idxs[suggest_idxs]
+
+        c = np.intersect1d(np.intersect1d(suggest_idxs, ulb_idxs), ulb_wrong)
+        w = np.setdiff1d(suggest_idxs, c)
+        rate.append(len(c) / budget)
+
+        # inject noise
+        c, w = add_noise(noise_rate, c, w)
+        correct = np.concatenate((correct, c), axis=0)
+        wrong = np.concatenate((wrong, w), axis=0)
+    print("Success Rate:\t{:.4f}".format(sum(rate)/len(rate)))
+    print(rate)
+    return rate
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, choices=["cifar10","mnist","fmnist"])
+parser.add_argument('--rate', type=int, choices=[30,10,20])
+parser.add_argument("--tolerance", type=float, help="Feedback noise")
+parser.add_argument("--budget", type=int)
+# parser.add_argument("--ignore", type=float, default=0.0)
+parser.add_argument("--init_round", type=int, default=10000)
+parser.add_argument("--round", type=int, default=10, help="Feedback round")
+args = parser.parse_args()
+
+# get hyperparameters
+DATASET = args.dataset
+RATE = args.rate
+BUDGET = args.budget
+TOLERANCE = args.tolerance
+# IGNORE_RATE = args.ignore
+ROUND = args.round
+INIT_ROUND = args.init_round
+
+
+# load meta data
+CONTENT_PATH = "/home/xianglin/projects/DVI_data/active_learning/random/resnet18/CIFAR10/{}".format(RATE)
+with open(os.path.join(CONTENT_PATH, "config.json"), "r") as f:
+    config = json.load(f)
+config = config["DVI"]
+
+CLASSES = config["CLASSES"]
+GPU_ID = config["GPU"]
+EPOCH_START = config["EPOCH_START"]
+EPOCH_END = config["EPOCH_END"]
+EPOCH_PERIOD = config["EPOCH_PERIOD"]
+
+# Training parameter (subject model)
+TRAINING_PARAMETER = config["TRAINING"]
+NET = TRAINING_PARAMETER["NET"]
+LEN = TRAINING_PARAMETER["train_num"]   # all
+
+# define hyperparameters
+DEVICE = torch.device("cuda:{}".format(GPU_ID) if torch.cuda.is_available() else "cpu")
+
+import Model.model as subject_model
+net = eval("subject_model.{}()".format(NET))
+data_provider = ActiveLearningDataProvider(CONTENT_PATH, net, EPOCH_START,device=DEVICE, classes=CLASSES, iteration_name="Epoch")
+
+# meta info
+lb_idxs = data_provider.get_labeled_idx(EPOCH_END)
+ulb_idxs = data_provider.get_unlabeled_idx(LEN, lb_idxs)
+
+data = data_provider.train_representation_all(EPOCH_END)
+labels = data_provider.train_labels_all(EPOCH_END)
+pred = data_provider.get_pred(EPOCH_END, data).argmax(1)
+wrong_pred_idx = np.argwhere(pred!=labels).squeeze()
+ulb_wrong = np.intersect1d(wrong_pred_idx, ulb_idxs)
+
+# evaluate
+with open(os.path.join(CONTENT_PATH, "Model", 'DVI_sample_recommender.pkl'), 'rb') as f:
+    dvi_tm = pickle.load(f)
+with open(os.path.join(CONTENT_PATH, "Model", 'TimeVis_sample_recommender.pkl'), 'rb') as f:
+    timevis_tm = pickle.load(f)
+
+#############################################
+#                   init                    #
+#############################################
+# random init
+print("Random sampling init")
+random_rate = list()
+pool = np.array(ulb_idxs)
+for _ in range(INIT_ROUND):
+    s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
+    random_rate.append(len(np.intersect1d(s_idxs, ulb_wrong))/BUDGET)
+print("Success Rate:\t{:.4f}".format(sum(random_rate)/len(random_rate)))
+
+# dvi init
+init_sampling(tm=dvi_tm, method="DVI", round=INIT_ROUND, budget=BUDGET)
+
+# timevis init
+init_sampling(tm=timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
+
+#############################################
+#                 Feedback                  #
+#############################################
+# random sampling
+print("Random sampling feedback:")
+random_rate = list()
+pool = np.array(ulb_idxs)
+for _ in range(ROUND):
+    s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
+    random_rate.append(len(np.intersect1d(s_idxs, ulb_wrong))/BUDGET)
+    pool = np.setdiff1d(pool, s_idxs)
+print("Success Rate:\t{:.4f}".format(sum(random_rate)/len(random_rate)))
+
+# dvi sampling
+feedback_sampling(tm=dvi_tm, method="DVI", round=ROUND, budget=BUDGET, noise_rate=0.0)
+
+# timevis sampling
+feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=0.0)
+
+#############################################
+#              Noise Feedback               #
+#############################################
+# dvi tolerance
+feedback_sampling(tm=dvi_tm, method="DVI", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
+
+# timevis tolerance
+feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
+
--- feedback_noise_test.py
+++ feedback_noise_test.py
@@ -5,6 +5,8 @@ import pickle
 import argparse
 
 def add_noise(rate, acc_idxs, rej_idxs):
+    if rate == 0:
+        return acc_idxs, rej_idxs
     acc_noise = np.random.choice(len(acc_idxs), size=len(acc_idxs)*rate//1)
     acc_noise = acc_idxs[acc_noise]
     new_acc = np.setdiff1d(acc_idxs, acc_noise)
@@ -17,20 +19,64 @@ def add_noise(rate, acc_idxs, rej_idxs):
     new_rej = np.concatenate((new_rej, acc_noise), axis=0)
     return new_acc, new_rej
 
+def init_sampling(tm, method, round, budget):
+    print("Feedback sampling initialization ({}):".format(method))
+    init_rate = list()
+    for _ in range(round):
+        correct = np.array([]).astype(np.int32)
+        wrong = np.array([]).astype(np.int32)
+        selected,_ = tm.sample_batch_init(correct, wrong, budget)
+        c = np.intersect1d(selected, noise_idxs)
+        init_rate.append(len(c)/budget)
+    print("Success Rate:\t{:.4f}".format(sum(init_rate)/len(init_rate)))
+    return sum(init_rate)/len(init_rate)
+
+def feedback_sampling(tm, method, round, budget, noise_rate=0.0):
+    print("Feedback sampling ({}) with noise {}:".format(method, noise_rate))
+    rate = list()
+    correct = np.array([]).astype(np.int32)
+    wrong = np.array([]).astype(np.int32)
+    selected,_ = tm.sample_batch_init(correct, wrong, BUDGET)
+    c = np.intersect1d(selected, noise_idxs)
+    w = np.setdiff1d(selected, c)
+    correct = np.concatenate((correct, c), axis=0)
+    wrong = np.concatenate((wrong, w), axis=0)
+    rate.append(len(correct)/budget)
+
+    # inject noise
+    correct, wrong = add_noise(noise_rate, correct, wrong)
+
+    for _ in range(round-1):
+        selected,_ = tm.sample_batch(correct, wrong, BUDGET)
+        c = np.intersect1d(selected, noise_idxs)
+        w = np.setdiff1d(selected, c)
+        rate.append(len(c)/BUDGET)
+
+        # inject noise
+        c, w = add_noise(TOLERANCE, c, w)
+
+        correct = np.concatenate((correct, c), axis=0)
+        wrong = np.concatenate((wrong, w), axis=0)
+    print("Success Rate:\t{:.4f}".format(rate))
+    print(rate)
+    return rate
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, choices=["cifar10","mnist","fmnist"])
 parser.add_argument('--noise_rate', type=int, choices=[5,10,20])
 parser.add_argument("--tolerance", type=float)
 parser.add_argument("--budget", type=int)
-
+parser.add_argument("--init_round", type=int, default=10000)
+parser.add_argument("--round", type=int, default=10, help="Feedback round")
 args = parser.parse_args()
 
 DATASET = args.dataset
 NOISE_RATE = args.noise_rate
-# VIS_METHOD = args.method
 BUDGET = args.budget
 TOLERANCE = args.tolerance
+ROUND = args.round
+INIT_ROUND = args.init_round
 
 CONTENT_PATH = "/home/xianglin/projects/DVI_data/noisy/symmetric/{}/{}/".format(DATASET, NOISE_RATE)
 with open(os.path.join(CONTENT_PATH, "config.json"), "r") as f:
@@ -54,39 +100,27 @@ with open(os.path.join(CONTENT_PATH,  'DVI_sample_recommender.pkl'), 'rb') as f:
 with open(os.path.join(CONTENT_PATH,  'TimeVis_sample_recommender.pkl'), 'rb') as f:
     timevis_tm = pickle.load(f)
 
-
+#############################################
+#                   init                    #
+#############################################
 # random init
 print("Random sampling init")
 s_rate = list()
 pool = np.arange(LEN)
-for _ in range(10000):
+for _ in range(INIT_ROUND):
     s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
     s_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
 print("Success Rate:\t{:.4f}".format(sum(s_rate)/len(s_rate)))
 
 # dvi init
-print("Feedback sampling initialization (DVI):")
-init_rate = list()
-for _ in range(10000):
-    correct = np.array([]).astype(np.int32)
-    wrong = np.array([]).astype(np.int32)
-    selected,_ = dvi_tm.sample_batch_init(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    init_rate.append(len(c)/BUDGET)
-print("Success Rate:\t{:.4f}".format(sum(init_rate)/len(init_rate)))
+init_sampling(dvi_tm, method="DVI", round=INIT_ROUND, budget=BUDGET)
 
 # timevis init
-print("Feedback sampling initialization (TimeVis):")
-init_rate = list()
-for _ in range(10000):
-    correct = np.array([]).astype(np.int32)
-    wrong = np.array([]).astype(np.int32)
-    selected,_ = timevis_tm.sample_batch_init(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    init_rate.append(len(c)/BUDGET)
-print("Success Rate:\t{:.4f}".format(sum(init_rate)/len(init_rate)))
+init_sampling(timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
 
-
+#############################################
+#                 Feedback                  #
+#############################################
 # random Feedback
 print("Random sampling feedback")
 random_rate = list()
@@ -96,88 +130,22 @@ for _ in range(11):
     random_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
     pool = np.setdiff1d(pool, s_idxs)
 print("Success Rate:\t{:.4f}".format(sum(random_rate)/len(random_rate)))
+print(random_rate)
 
 # dvi Feedback
-print("Feedback sampling (DVI):")
-dvi_rate = list()
-correct = np.array([]).astype(np.int32)
-wrong = np.array([]).astype(np.int32)
-selected,_ = dvi_tm.sample_batch_init(correct, wrong, BUDGET)
-c = np.intersect1d(selected, noise_idxs)
-w = np.setdiff1d(selected, c)
-correct = np.concatenate((correct, c), axis=0)
-wrong = np.concatenate((wrong, w), axis=0)
-for _ in range(10):
-    selected,_ = dvi_tm.sample_batch(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    w = np.setdiff1d(selected, c)
-    dvi_rate.append(len(c)/BUDGET)
-    correct = np.concatenate((correct, c), axis=0)
-    wrong = np.concatenate((wrong, w), axis=0)
-print("Success Rate:\t{:.4f}".format(sum(dvi_rate)/len(dvi_rate)))
-print(dvi_rate)
+feedback_sampling(tm=dvi_tm, method="DVI", round=ROUND, budget=BUDGET)
 
 # timevis Feedback
-print("Feedback sampling (TimeVis):")
-timevis_rate = list()
-correct = np.array([]).astype(np.int32)
-wrong = np.array([]).astype(np.int32)
-selected,_ = timevis_tm.sample_batch_init(correct, wrong, BUDGET)
-c = np.intersect1d(selected, noise_idxs)
-w = np.setdiff1d(selected, c)
-correct = np.concatenate((correct, c), axis=0)
-wrong = np.concatenate((wrong, w), axis=0)
-for _ in range(10):
-    selected,_ = timevis_tm.sample_batch(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    w = np.setdiff1d(selected, c)
-    timevis_rate.append(len(c)/BUDGET)
-    correct = np.concatenate((correct, c), axis=0)
-    wrong = np.concatenate((wrong, w), axis=0)
-print("Success Rate:\t{:.4f}".format(sum(timevis_rate)/len(timevis_rate)))
-print(timevis_rate)
+feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET)
 
 
-# dvi tolerance
-print("Feedback sampling (DVI) with noise:")
-dvi_with_noise = list()
-correct = np.array([]).astype(np.int32)
-wrong = np.array([]).astype(np.int32)
-selected,_ = dvi_tm.sample_batch_init(correct, wrong, BUDGET)
-c = np.intersect1d(selected, noise_idxs)
-w = np.setdiff1d(selected, c)
-correct = np.concatenate((correct, c), axis=0)
-wrong = np.concatenate((wrong, w), axis=0)
-for _ in range(10):
-    selected,_ = dvi_tm.sample_batch(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    w = np.setdiff1d(selected, c)
-    dvi_with_noise.append(len(c)/BUDGET)
-    # add noise
-    c, w = add_noise(TOLERANCE, c, w)
-    correct = np.concatenate((correct, c), axis=0)
-    wrong = np.concatenate((wrong, w), axis=0)
-print("Success Rate:\t{:.4f}".format(sum(dvi_with_noise)/len(dvi_with_noise)))
-print(dvi_with_noise)
+#############################################
+#              Noise Feedback               #
+#############################################
 
-# timevis tolerance
-print("Feedback sampling (TimeVis) with noise:")
-timevis_with_noise = list()
-correct = np.array([]).astype(np.int32)
-wrong = np.array([]).astype(np.int32)
-selected,_ = timevis_tm.sample_batch_init(correct, wrong, BUDGET)
-c = np.intersect1d(selected, noise_idxs)
-w = np.setdiff1d(selected, c)
-correct = np.concatenate((correct, c), axis=0)
-wrong = np.concatenate((wrong, w), axis=0)
-for _ in range(10):
-    selected,_ = timevis_tm.sample_batch(correct, wrong, BUDGET)
-    c = np.intersect1d(selected, noise_idxs)
-    w = np.setdiff1d(selected, c)
-    timevis_with_noise.append(len(c)/BUDGET)
-    # add noise
-    c, w = add_noise(TOLERANCE, c, w)
-    correct = np.concatenate((correct, c), axis=0)
-    wrong = np.concatenate((wrong, w), axis=0)
-print("Success Rate:\t{:.4f}".format(sum(timevis_with_noise)/len(timevis_with_noise)))
-print(timevis_with_noise)
+# dvi Feedback with noise
+feedback_sampling(tm=dvi_tm, method="DVI", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
+
+# timevis Feedback with noise
+feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
+
