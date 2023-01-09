ed15bbaa622b0ef4dde6a7475f94fed1d6f6a436
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-09 22:36:28 +0800

    record data from noise setting

--- feedback_noise_test.py
+++ feedback_noise_test.py
@@ -3,6 +3,7 @@ import os
 import time
 import json
 import pickle
+import pandas as pd
 import argparse
 
 def add_noise(rate, acc_idxs, rej_idxs):
@@ -32,7 +33,7 @@ def init_sampling(tm, method, round, budget):
     print("Success Rate:\t{:.4f}".format(sum(init_rate)/len(init_rate)))
     return sum(init_rate)/len(init_rate)
 
-def feedback_sampling(tm, method, round, budget, noise_rate=0.0):
+def feedback_sampling(tm, method, round, budget, noise_rate=0.0, replace_init=None):
     print("--------------------------------------------------------")
     print("({}) with noise rate {}:\n".format(method, noise_rate))
     rate = np.zeros(round)
@@ -43,7 +44,10 @@ def feedback_sampling(tm, method, round, budget, noise_rate=0.0):
     w = np.setdiff1d(selected, c)
     correct = np.concatenate((correct, c), axis=0)
     wrong = np.concatenate((wrong, w), axis=0)
-    rate[0] = len(correct)/float(budget)
+    if replace_init is None:
+        rate[0] = len(correct)/float(budget)
+    else:
+        rate[0] = replace_init
     # inject noise
     correct, wrong = add_noise(noise_rate, correct, wrong)
 
@@ -59,7 +63,7 @@ def feedback_sampling(tm, method, round, budget, noise_rate=0.0):
         wrong = np.concatenate((wrong, w), axis=0)
     
     ac_rate = np.array([rate[:i].mean() for i in range(1, len(rate)+1)])
-    print("Success Rate:{:.3f}\n{}\n".format(ac_rate[-1], ac_rate))
+    # print("Success Rate:{:.3f}\n{}\n".format(ac_rate[-1], ac_rate))
     print("Feature Importance:\t{}\n".format(coef_))
     return ac_rate
 
@@ -96,13 +100,20 @@ def feedback_sampling_efficiency(tm, method, round, budget, repeat, noise_rate=0
         all_time_cost = all_time_cost + time_cost  
     all_time_cost = all_time_cost/repeat
     print("Time Cost:\n{}\n".format(all_time_cost)) 
-    return all_time_cost/repeat
+    return all_time_cost
 
+def record(old_array, to_be_record, task, dataset, method, rate, tolerance):
+    for i, v in enumerate(to_be_record, start=1):
+        if old_array is None:
+            old_array = np.array([task, dataset, method, rate, tolerance, str(i), str(v)])
+        else:
+            old_array = np.vstack((old_array, np.array([task, dataset, method, rate, tolerance, str(i), str(v)])))
+    return old_array
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, choices=["cifar10","mnist","fmnist"])
 parser.add_argument('--noise_rate', type=int, choices=[5,10,20])
-parser.add_argument("--tolerance", type=float)
+parser.add_argument("--tolerance", nargs="+", type=float)
 parser.add_argument('--repeat', type=int, default=100, help="repeat x times to evaluate efficiency")
 parser.add_argument("--budget", type=int, default=50)
 parser.add_argument("--init_round", type=int, default=10000)
@@ -143,23 +154,25 @@ with open(os.path.join(CONTENT_PATH, 'tfDVI_sample_recommender.pkl'), 'rb') as f
 with open(os.path.join(CONTENT_PATH, 'TimeVis_sample_recommender.pkl'), 'rb') as f:
     timevis_tm = pickle.load(f)
 
-# #############################################
-# #                   init                    #
-# #############################################
-# # random init
-# print("Random sampling init")
-# s_rate = list()
-# pool = np.arange(LEN)
-# for _ in range(INIT_ROUND):
-#     s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
-#     s_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
-# print("Success Rate:\t{:.4f}".format(sum(s_rate)/len(s_rate)))
+data = None
+#############################################
+#                   init                    #
+#############################################
+# random init
+print("Random sampling init")
+s_rate = list()
+pool = np.arange(LEN)
+for _ in range(INIT_ROUND):
+    s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
+    s_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
+print("Success Rate:\t{:.4f}".format(sum(s_rate)/len(s_rate)))
+random_init = sum(s_rate)/len(s_rate)
 
-# # dvi init
-# init_sampling(dvi_tm, method="tfDVI", round=INIT_ROUND, budget=BUDGET)
+# dvi init
+dvi_init = init_sampling(dvi_tm, method="tfDVI", round=INIT_ROUND, budget=BUDGET)
 
-# # timevis init
-# init_sampling(timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
+# timevis init
+timevis_init = init_sampling(timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
 
 #############################################
 #                 Feedback                  #
@@ -173,34 +186,54 @@ for r in range(ROUND):
     s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
     random_rate[r] = len(np.intersect1d(s_idxs, noise_idxs))/BUDGET
     pool = np.setdiff1d(pool, s_idxs)
+random_rate[0] = random_init
 ac_random_rate = np.array([random_rate[:i].mean() for i in range(1, len(random_rate)+1)])
 print("Random Success Rate:{:.3f}\n{}\n".format(ac_random_rate[-1], ac_random_rate))
+data = record(data, ac_random_rate, "feedback", DATASET, "Random", NOISE_RATE, 0.0)
 
 # dvi Feedback
-feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET)
+ac_dvi_rate = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, replace_init=dvi_init)
+data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, 0.0)
 
 # timevis Feedback
-feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET)
-
+ac_tv_rate = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, replace_init=timevis_init)
+data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, 0.0)
 
 #############################################
 #              Noise Feedback               #
 #############################################
 
-# dvi Feedback with noise
-feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
+for tol in TOLERANCE:
+    # dvi Feedback with noise
+    ac_dvi_rate = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=dvi_init)
+    data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, tol)
 
-# timevis Feedback with noise
-feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=TOLERANCE)
-
+    # timevis Feedback with noise
+    ac_tv_rate = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=timevis_init)
+    data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, tol)
 
 #############################################
 #            Feedback Efficiency            #
 #############################################
 
 # dvi Feedback
-feedback_sampling_efficiency(tm=dvi_tm, method="tfDVI", round=ROUND, budget=5000, repeat=REPEAT)
+dvi_c = feedback_sampling_efficiency(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, repeat=REPEAT)
+data = record(data, dvi_c, "efficiency", DATASET, "DVI", NOISE_RATE, 0.0)
 
 # timevis Feedback
-feedback_sampling_efficiency(tm=timevis_tm, method="TimeVis", round=ROUND, budget=5000, repeat=REPEAT)
+timevis_c = feedback_sampling_efficiency(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, repeat=REPEAT)
+data = record(data, timevis_c, "efficiency", DATASET, "TimeVis", NOISE_RATE, 0.0)
 
+#############################################
+#                    Save                   #
+#############################################
+# read results
+eval_path = "/home/xianglin/projects/DVI_data/noisy/symmetric/feedback.xlsx"
+col = np.array(["task", "dataset", "method", "rate", "tolerance", "iter", "eval"])
+if os.path.exists(eval_path):
+    df = pd.read_excel(eval_path, index_col=0, dtype={"task":str, "dataset":str, "method":str, "rate":int, "tolerance":float, "iter":int, "eval":float})
+else:
+    df = pd.DataFrame({}, columns=col)
+df_curr = pd.DataFrame(data, columns=col)
+df = df.append(df_curr, ignore_index=True)
+df.to_excel(eval_path)
