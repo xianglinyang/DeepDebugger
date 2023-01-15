c6b0b89c115845c976139a251d2818e9109c5ebe
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-15 22:27:51 +0800

    minor

--- batch_run.py
+++ batch_run.py
@@ -74,15 +74,17 @@ import os
 #         for method in methods:
 #             os.system("python feedback_al.py --dataset {} --rate {} --method {}".format(data.upper(), rate, method))
 
-# feedback test on noisy dataset
-datasets = ["mnist", "fmnist", "cifar10"]
-rates = ["5", "10", "20"]
-for data in datasets:
-    for rate in rates:
-        os.system("python feedback_noise_test.py --dataset {} --noise_rate {} --tolerance 0.03 0.05 0.1 0.15 --repeat 100".format(data, rate))
+repeat = 500
+for _ in range(repeat):
+    # feedback test on noisy dataset
+    datasets = ["mnist", "fmnist", "cifar10"]
+    rates = ["5", "10", "20"]
+    for data in datasets:
+        for rate in rates:
+            os.system("python feedback_noise_test.py --dataset {} --noise_rate {} --tolerance 0.03 0.05 0.1 0.15 --repeat 1 --round 50".format(data, rate))
 
-# feedback test on active learning dataset
-rates = ["10", "20", "30"]
-for data in datasets:
-    for rate in rates:
-        os.system("python feedback_al_test.py --dataset {} --rate {} --tolerance 0.03 0.05 0.1 0.15 --repeat 100".format(data, rate))
+    # feedback test on active learning dataset
+    rates = ["10", "20", "30"]
+    for data in datasets:
+        for rate in rates:
+            os.system("python feedback_al_test.py --dataset {} --rate {} --tolerance 0.03 0.05 0.1 0.15 --repeat 1 --round 50".format(data, rate))
