4bf460257e4cbcde63c8c82680a239f6a5547a8b
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-20 09:55:50 +0800

    update

--- feedback_noise_test.py
+++ feedback_noise_test.py
@@ -178,103 +178,107 @@ with open(os.path.join(CONTENT_PATH, 'TimeVis_sample_recommender.pkl'), 'rb') as
 # print("Accelera ranking corr:\t{:.3f}".format(a_corr))
 
 data = None
-# #############################################
-# #                   init                    #
-# #############################################
-# # random init
-# print("Random sampling init")
-# s_rate = list()
-# pool = np.arange(LEN)
-# for _ in range(INIT_ROUND):
-#     s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
-#     s_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
-# print("Success Rate:\t{:.4f}".format(sum(s_rate)/len(s_rate)))
-# random_init = sum(s_rate)/len(s_rate)
+#############################################
+#                   init                    #
+#############################################
+# random init
+print("Random sampling init")
+s_rate = list()
+pool = np.arange(LEN)
+for _ in range(INIT_ROUND):
+    s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
+    s_rate.append(len(np.intersect1d(s_idxs, noise_idxs))/BUDGET)
+print("Success Rate:\t{:.4f}".format(sum(s_rate)/len(s_rate)))
+random_init = sum(s_rate)/len(s_rate)
 
-# # dvi init
-# dvi_init = init_sampling(dvi_tm, method="tfDVI", round=INIT_ROUND, budget=BUDGET)
+# dvi init
+dvi_init = init_sampling(dvi_tm, method="tfDVI", round=INIT_ROUND, budget=BUDGET)
 
-# # timevis init
-# timevis_init = init_sampling(timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
+# timevis init
+timevis_init = init_sampling(timevis_tm, method="TimeVis", round=INIT_ROUND, budget=BUDGET)
 
-# #############################################
-# #                 Feedback                  #
-# #############################################
-# # random Feedback
-# print("--------------------------------------------------------")
-# print("Random sampling feedback:\n")
-# random_rate = np.zeros(ROUND)
-# pool = np.arange(LEN)
-# for r in range(ROUND):
-#     s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
-#     random_rate[r] = len(np.intersect1d(s_idxs, noise_idxs))/BUDGET
-#     pool = np.setdiff1d(pool, s_idxs)
-# random_rate[0] = random_init
-# ac_random_rate = np.array([random_rate[:i].mean() for i in range(1, len(random_rate)+1)])
-# print("Random Success Rate:{:.3f}\n{}\n".format(ac_random_rate[-1], ac_random_rate))
-# data = record(data, ac_random_rate, "feedback", DATASET, "Random", NOISE_RATE, 0.0)
+#############################################
+#                 Feedback                  #
+#############################################
+# random Feedback
+print("--------------------------------------------------------")
+print("Random sampling feedback:\n")
+random_rate = np.zeros(ROUND)
+pool = np.arange(LEN)
+for r in range(ROUND):
+    s_idxs = np.random.choice(pool,size=BUDGET,replace=False)
+    random_rate[r] = len(np.intersect1d(s_idxs, noise_idxs))/BUDGET
+    pool = np.setdiff1d(pool, s_idxs)
+random_rate[0] = random_init
+ac_random_rate = np.array([random_rate[:i].mean() for i in range(1, len(random_rate)+1)])
+print("Random Success Rate:{:.3f}\n{}\n".format(ac_random_rate[-1], ac_random_rate))
+data = record(data, ac_random_rate, "feedback", DATASET, "Random", NOISE_RATE, 0.0)
 
-# # dvi Feedback
-# ac_dvi_rate, dvi_coef_ = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, replace_init=dvi_init)
-# # data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, 0.0)
-# data = record(data, dvi_coef_, "FI", DATASET, "DVI", NOISE_RATE, 0.0)
+# dvi Feedback
+ac_dvi_rate, dvi_coef_ = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, replace_init=dvi_init)
+data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, 0.0)
+data = record(data, dvi_coef_, "FI", DATASET, "DVI", NOISE_RATE, 0.0)
 
-# # timevis Feedback
-# ac_tv_rate, tv_coef_ = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, replace_init=timevis_init)
-# # data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, 0.0)
-# data = record(data, tv_coef_, "FI", DATASET, "TimeVis", NOISE_RATE, 0.0)
+# timevis Feedback
+ac_tv_rate, tv_coef_ = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, replace_init=timevis_init)
+data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, 0.0)
+data = record(data, tv_coef_, "FI", DATASET, "TimeVis", NOISE_RATE, 0.0)
 
 
-# #############################################
-# #              Noise Feedback               #
-# #############################################
+#############################################
+#              Noise Feedback               #
+#############################################
 
-# for tol in TOLERANCE:
-#     # dvi Feedback with noise
-#     ac_dvi_rate, _ = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=dvi_init)
-#     data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, tol)
+for tol in TOLERANCE:
+    # dvi Feedback with noise
+    ac_dvi_rate, dvi_coef_ = feedback_sampling(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=dvi_init)
+    data = record(data, ac_dvi_rate, "feedback", DATASET, "DVI", NOISE_RATE, tol)
+    data = record(data, dvi_coef_, "FI", DATASET, "DVI", NOISE_RATE, tol)
 
-#     # timevis Feedback with noise
-#     ac_tv_rate, _ = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=timevis_init)
-#     data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, tol)
+    # timevis Feedback with noise
+    ac_tv_rate, tv_coef_ = feedback_sampling(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, noise_rate=tol, replace_init=timevis_init)
+    data = record(data, ac_tv_rate, "feedback", DATASET, "TimeVis", NOISE_RATE, tol)
+    data = record(data, tv_coef_, "FI", DATASET, "TimeVis", NOISE_RATE, tol)
 
-# #############################################
-# #            Feedback Efficiency            #
-# #############################################
+#############################################
+#            Feedback Efficiency            #
+#############################################
 
-# # dvi Feedback
-# dvi_c = feedback_sampling_efficiency(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, repeat=REPEAT)
-# data = record(data, dvi_c, "efficiency", DATASET, "DVI", NOISE_RATE, 0.0)
+# dvi Feedback
+dvi_c = feedback_sampling_efficiency(tm=dvi_tm, method="tfDVI", round=ROUND, budget=BUDGET, repeat=REPEAT)
+data = record(data, dvi_c, "efficiency", DATASET, "DVI", NOISE_RATE, 0.0)
 
-# # timevis Feedback
-# timevis_c = feedback_sampling_efficiency(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, repeat=REPEAT)
-# data = record(data, timevis_c, "efficiency", DATASET, "TimeVis", NOISE_RATE, 0.0)
+# timevis Feedback
+timevis_c = feedback_sampling_efficiency(tm=timevis_tm, method="TimeVis", round=ROUND, budget=BUDGET, repeat=REPEAT)
+data = record(data, timevis_c, "efficiency", DATASET, "TimeVis", NOISE_RATE, 0.0)
 
-# #############################################
-# #              Random Anomaly               #
-# #############################################
-# xs = dvi_tm.embeddings_2d[:, -dvi_tm.period:, 0]
-# ys = dvi_tm.embeddings_2d[:, -dvi_tm.period:, 1]
-# new_sample = generate_random_trajectory(xs.min(), ys.min(), xs.max(), ys.max(), dvi_tm.period)
-# dvi_new_score = dvi_tm.score_new_sample(new_sample)
-# data = record(data, dvi_new_score, "RA", DATASET, "DVI", NOISE_RATE, 0.0)
+#############################################
+#              Random Anomaly               #
+#############################################
+xs = dvi_tm.embeddings_2d[:, -dvi_tm.period:, 0]
+ys = dvi_tm.embeddings_2d[:, -dvi_tm.period:, 1]
+for _ in range(25):
+    new_sample = generate_random_trajectory(xs.min(), ys.min(), xs.max(), ys.max(), dvi_tm.period)
+    dvi_new_score = dvi_tm.score_new_sample(new_sample)
+    data = record(data, dvi_new_score, "RA", DATASET, "DVI", NOISE_RATE, 0.0)
 
-# xs = timevis_tm.embeddings_2d[:, -timevis_tm.period:, 0]
-# ys = timevis_tm.embeddings_2d[:, -timevis_tm.period:, 1]
-# new_sample = generate_random_trajectory(xs.min(), ys.min(), xs.max(), ys.max(), timevis_tm.period)
-# tv_new_score = timevis_tm.score_new_sample(new_sample)
-# data = record(data, tv_new_score, "RA", DATASET, "TimeVis", NOISE_RATE, 0.0)
+xs = timevis_tm.embeddings_2d[:, -timevis_tm.period:, 0]
+ys = timevis_tm.embeddings_2d[:, -timevis_tm.period:, 1]
+for _ in range(25):
+    new_sample = generate_random_trajectory(xs.min(), ys.min(), xs.max(), ys.max(), timevis_tm.period)
+    tv_new_score = timevis_tm.score_new_sample(new_sample)
+    data = record(data, tv_new_score, "RA", DATASET, "TimeVis", NOISE_RATE, 0.0)
 
-# #############################################
-# #                    Save                   #
-# #############################################
-# # read results
-# eval_path = "/home/xianglin/projects/DVI_data/noisy/symmetric/feedback.xlsx"
-# col = np.array(["task", "dataset", "method", "rate", "tolerance", "iter", "eval"])
-# if os.path.exists(eval_path):
-#     df = pd.read_excel(eval_path, index_col=0, dtype={"task":str, "dataset":str, "method":str, "rate":int, "tolerance":float, "iter":int, "eval":float})
-# else:
-#     df = pd.DataFrame({}, columns=col)
-# df_curr = pd.DataFrame(data, columns=col)
-# df = df.append(df_curr, ignore_index=True)
-# df.to_excel(eval_path)
+#############################################
+#                    Save                   #
+#############################################
+# read results
+eval_path = "/home/xianglin/projects/DVI_data/noisy/symmetric/feedback.xlsx"
+col = np.array(["task", "dataset", "method", "rate", "tolerance", "iter", "eval"])
+if os.path.exists(eval_path):
+    df = pd.read_excel(eval_path, index_col=0, dtype={"task":str, "dataset":str, "method":str, "rate":int, "tolerance":float, "iter":int, "eval":float})
+else:
+    df = pd.DataFrame({}, columns=col)
+df_curr = pd.DataFrame(data, columns=col)
+df = df.append(df_curr, ignore_index=True)
+df.to_excel(eval_path)
