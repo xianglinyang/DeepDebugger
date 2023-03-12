c3f571a399f4335b08c2985e0e2e4bf7da93ee33
Yang Xianglin <yxlyzxl@gmail.com>
2023-03-12 22:01:30 +0800

    projector for dense al

--- singleVis/projector.py
+++ singleVis/projector.py
@@ -92,9 +92,9 @@ class DeepDebuggerProjector(Projector):
         print("Successfully load the visualization model for range ({},{})...".format(s,e))
 
 
-class ALProjector(DeepDebuggerProjector):
+class ALProjector(Projector):
     def __init__(self, vis_model, content_path, vis_model_name, device) -> None:
-        super().__init__(vis_model, content_path,vis_model_name, None, device)
+        super().__init__(vis_model, content_path,vis_model_name, device)
         self.current_range = None
 
     def load(self, iteration):
@@ -205,6 +205,7 @@ class DVIProjector(Projector):
         self.vis_model.eval()
         print("Successfully load the DVI visualization model for iteration {}".format(iteration))
 
+
 class TimeVisProjector(Projector):
     def __init__(self, vis_model, content_path, vis_model_name, device, verbose=0) -> None:
         super().__init__(vis_model, content_path, vis_model_name, device)
@@ -219,8 +220,48 @@ class TimeVisProjector(Projector):
         if self.verbose>0:
             print("Successfully load the TimeVis visualization model for iteration {}".format(iteration))
 
-import tensorflow as tf
 
+class TimeVisDenseALProjector(Projector):
+    def __init__(self, vis_model, content_path, vis_model_name, device, verbose=0) -> None:
+        super().__init__(vis_model, content_path, vis_model_name, device)
+        self.verbose = verbose
+        self.curr_iteration = -1
+
+    def load(self, iteration, epoch):
+        if iteration == self.curr_iteration:
+            return
+        file_path = os.path.join(self.content_path, "Model", f'Iteration_{iteration}', "{}.pth".format(self.vis_model_name))
+        save_model = torch.load(file_path, map_location="cpu")
+        self.vis_model.load_state_dict(save_model["state_dict"])
+        self.vis_model.to(self.DEVICE)
+        self.vis_model.eval()
+        if self.verbose>0:
+            print("Successfully load the TimeVis visualization model for iteration {}".format(iteration))
+        self.curr_iteration = iteration
+        
+    
+    def batch_project(self, iteration, epoch, data):
+        self.load(iteration, epoch)
+        embedding = self.vis_model.encoder(torch.from_numpy(data).to(dtype=torch.float32, device=self.DEVICE)).cpu().detach().numpy()
+        return embedding
+    
+    def individual_project(self, iteration, epoch, data):
+        self.load(iteration, epoch)
+        embedding = self.vis_model.encoder(torch.from_numpy(np.expand_dims(data, axis=0)).to(dtype=torch.float32, device=self.DEVICE)).cpu().detach().numpy()
+        return embedding.squeeze(axis=0)
+    
+    def batch_inverse(self, iteration, epoch, embedding):
+        self.load(iteration, epoch)
+        data = self.vis_model.decoder(torch.from_numpy(embedding).to(dtype=torch.float32, device=self.DEVICE)).cpu().detach().numpy()
+        return data
+    
+    def individual_inverse(self, iteration, epoch, embedding):
+        self.load(iteration, epoch)
+        data = self.vis_model.decoder(torch.from_numpy(np.expand_dims(embedding, axis=0)).to(dtype=torch.float32, device=self.DEVICE)).cpu().detach().numpy()
+        return data.squeeze(axis=0)
+
+
+import tensorflow as tf
 class tfDVIProjector(ProjectorAbstractClass):
     def __init__(self, content_path, flag, verbose=0):
         self.content_path = content_path
@@ -288,6 +329,80 @@ class tfDVIProjector(ProjectorAbstractClass):
         :return: high dim representation, numpy.ndarray
         """
         self.load(epoch)
+        data = np.expand_dims(data, axis=0)
+        representation_data = self.decoder(data).cpu().numpy()
+        return representation_data.squeeze(0)
+
+
+class tfDVIDenseALProjector(ProjectorAbstractClass):
+    def __init__(self, content_path, flag, verbose=0):
+        self.content_path = content_path
+        self.model_path = os.path.join(self.content_path, "Model")
+        self.flag = flag
+        self.curr_iteration = -1
+        self.curr_epoch = -1
+        self.encoder = None
+        self.decoder = None
+        self.verbose = verbose
+
+    def load(self, iteration, epoch):
+        if self.curr_iteration == iteration and self.curr_epoch == epoch:
+            print("Current autocoder model loaded from Iteration {}/Epoch {}".format(iteration, epoch))
+            return
+        encoder_location = os.path.join(self.model_path, "Iteration_{:d}".format(iteration), "Epoch_{:d}".format(epoch),"encoder" + self.flag)
+        decoder_location = os.path.join(self.model_path, "Iteration_{:d}".format(iteration), "Epoch_{:d}".format(epoch),"decoder" + self.flag)
+        try:
+            self.encoder = tf.keras.models.load_model(encoder_location)
+            self.decoder = tf.keras.models.load_model(decoder_location)
+            if self.verbose>0:
+                print("Keras autocoder model loaded from Epoch {}".format(epoch))
+            self.curr_iteration = iteration
+            self.curr_epoch = epoch
+        except FileNotFoundError:
+            print("Error! Projection function has not been initialized! Pls first visualize all.")
+
+    def batch_project(self, iteration, epoch, data):
+        '''
+        batch project data to 2D space
+        :param data: numpy.ndarray
+        :param epoch: int
+        :return: embedding numpy.ndarray
+        '''
+        self.load(iteration, epoch)
+        embedding = self.encoder(data).cpu().numpy()
+        return embedding
+
+    def individual_project(self, iteration, epoch, data):
+        '''
+        project a data to 2D space
+        :param data: numpy.ndarray
+        :param epoch: int
+        :return: embedding numpy.ndarray
+        '''
+        self.load(iteration, epoch)
+        data = np.expand_dims(data, axis=0)
+        embedding = self.encoder(data).cpu().numpy()
+        return embedding.squeeze(0)
+
+    def batch_inverse(self, iteration, epoch, data):
+        """
+        map 2D points back into high dimensional space
+        :param data: ndarray, (n, 2)
+        :param epoch: num of epoch
+        :return: high dim representation, numpy.ndarray
+        """
+        self.load(iteration, epoch)
+        representation_data = self.decoder(data).cpu().numpy()
+        return representation_data
+
+    def individual_inverse(self, iteration, epoch, data):
+        """
+        map a 2D point back into high dimensional space
+        :param data: ndarray, (1, 2)
+        :param epoch: num of epoch
+        :return: high dim representation, numpy.ndarray
+        """
+        self.load(iteration, epoch)
         data = np.expand_dims(data, axis=0)
         representation_data = self.decoder(data).cpu().numpy()
         return representation_data.squeeze(0)
